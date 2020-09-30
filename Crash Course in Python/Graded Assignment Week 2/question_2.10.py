14ae0e693dc140bd7fc5ab255ddb7ca8f75bcd91
tannuyadav03 <72121871+tannuyadav03@users.noreply.github.com>
2020-09-30 14:43:09 +0530

    Add files via upload

---
+++ Crash Course in Python/Graded Assignment Week 2/question_2.10.py
@@ -0,0 +1,14 @@
+def fractional_part(numerator, denominator):
+	ans1 = (numerator // denominator)
+	ans2 = (numerator / denominator)
+    print ( ans1 - ans2 )
+	# Operate with numerator and denominator to 
+# keep just the fractional part of the quotient
+	return 0
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
